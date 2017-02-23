1606b1ebf24e0b0322fd0106cb1d5fdff4719158
Hector Sevilla Sandoval <barcadict@MacBook-Pro-de-Hector.local>
2017-02-23 10:51:01 -0600

    Added home icons and footer

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,9 +17,19 @@ jQuery(function($){
             });
 
 
-  $('.flexslider').flexslider({
+  $('.slider__main').flexslider({
       animation: "slide",
       controlNav: false
   });
 
+/*
+  $('.carousel').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    minItems: 2,
+    maxItems: 4
+  });
+*/
 });
