8caa558ca4f42387e6e127e20469fa6e3c8fdc25
Hector Sevilla Sandoval <barcadict@MacBook-Pro-de-Hector.local>
2017-02-23 21:37:44 -0600

    Finishing up product detail

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,6 +22,27 @@ jQuery(function($){
       controlNav: false
   });
 
+  // The slider being synced must be initialized first
+  $('.product__carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '.product__mainPhoto'
+  });
+
+  $('.product__mainPhoto').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: ".product__carousel"
+  });
+
+  $("#tabs").tabs();
+
 /*
   $('.carousel').flexslider({
     animation: "slide",
